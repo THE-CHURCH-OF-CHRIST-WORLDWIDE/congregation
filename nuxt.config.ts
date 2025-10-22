c2828e58356da0c84d0fa51275c6b6cc1653ca88
Mfonido Mark <favourmark05@gmail.com>
2025-10-22 14:40:14 +0100

    feat: add Iconify integration and create index page
    - Added @iconify/vue for icon support in the project.
    - Created a new index.vue page with a welcome message and icons.
    - Introduced a plugin to register the Icon component globally.
    - Added TypeScript definitions for the Icon component.
    - Updated package.json to include new dependencies and devDependencies.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,67 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  ssr: false,
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  modules: [
+    '@pinia/nuxt',
+  ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  runtimeConfig: {
+    // Private keys (server only)
+    apiSecret: process.env.API_SECRET,
+
+    public: {
+      apiBase: process.env.API_BASE || "https://api.example.com",
+      appEnv: process.env.APP_ENV || "development",
+    },
+  },
+  components: [
+        {
+        path: '~/components',
+        pathPrefix: false,
+        },
+    ],
+    typescript: {
+    strict: true,
+    typeCheck: true,
+  },
+  pinia: {
+    storesDirs: ["./stores/**"]
+  },
+  app: {
+        pageTransition: { name: "fade", mode: "out-in" },
+        head: {
+            title: "Congregation",
+            titleTemplate: "%s - Congregation Marketplace",
+            meta: [
+                { name: "description", content: "Congregation - Your All in one church management solution" },
+                { name: "viewport", content: "width=device-width, initial-scale=1" },
+            ],
+            link: [
+                { rel: "icon", type: "image/png", href: "/images/icon.png" },
+                { rel: "icon", type: "image/x-icon", href: "/images/icon.png" },
+                { rel: "apple-touch-icon", href: "/images/icon.png" },
+                {
+                rel: "stylesheet",
+                href: "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap",
+                },
+                {
+                rel: "stylesheet",
+                href: "https://fonts.googleapis.com/css2?family=Piedra&display=swap",
+                },
+                {
+                rel: "stylesheet",
+                href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap",
+                },
+            ],
+        },
+    },
+});
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center bg-red-400 text-white p-20 flex gap-4 mx-auto justify-center items-center">
+    welcome to congregation
+    <Icon icon="mdi:account" />
+<Icon icon="logos:nuxt-icon" />
+<Icon icon="uil:github" />
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
